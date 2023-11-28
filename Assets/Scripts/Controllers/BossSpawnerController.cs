097a15adb9ffec90888073fc1c40d5419ed9a9b9
Elizabeth Liu <esl1350@gmail.com>
2023-11-28 16:57:53 -0500

    screen shake on boss

--- Assets/Scripts/Controllers/BossSpawnerController.cs
+++ Assets/Scripts/Controllers/BossSpawnerController.cs
@@ -22,10 +22,13 @@ public class BossSpawnerController : MonoBehaviour
 
     IEnumerator playAnimationThenSpawn() {
         animator.SetBool("spawn", true);
+        GameObject.FindWithTag("CMCam").GetComponent<CameraShake>().Shake(1.5f, 4f);
         yield return new WaitForSeconds(4);
         //please don't question the math, i didn't have time to figure out why it wouldn't spawn on top T u T
         //+ new Vector3(2f, 4.787f, 0f)
         GameObject boss = Instantiate(lich, this.transform.position + new Vector3(0f, 3f, 0f), Quaternion.identity);
+        GameObject.FindWithTag("CMCam").GetComponent<CameraShake>().Shake(3f, 0.2f);
+
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
         sr.sortingOrder = 0;
         yield return new WaitForSeconds(1);

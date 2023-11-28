06a6bea3c8886c7562d47471958ec3720279960c
AllisanLu <alcp2610@gmail.com>
2023-11-28 03:45:26 -0500

    Boss animations

--- Assets/Scripts/Controllers/LichController.cs
+++ Assets/Scripts/Controllers/LichController.cs
@@ -24,7 +24,7 @@ public class LichController : Controller
 
     bool first = false;
     public bool start = false;
-    public Sprite[] sprites;
+    float lastTP = 0;
 
     void Start()
     {
@@ -44,40 +44,74 @@ public class LichController : Controller
 
     private void Update()
     {
-        if (start) {
+        if (start)
+        {
             GameObject player = GameObject.FindGameObjectWithTag("Player");
             Vector2 vectordist = player.transform.position - this.transform.position;
             //find angle and choose sprite
             float angle = Mathf.Atan2(vectordist.y, vectordist.x) * Mathf.Rad2Deg;
-            if (angle < 0) {
+            if (angle < 0)
+            {
                 angle = 360 + angle;
             }
             int math = (int)((angle + 45) / 90) - 1;
             int spriteNo = ((math % 4) + 4) % 4;
-            SpriteRenderer renderer = GetComponentInChildren<SpriteRenderer>();
-            renderer.sprite = sprites[spriteNo];
+            //SpriteRenderer renderer = GetComponentInChildren<SpriteRenderer>();
+            //renderer.sprite = sprites[spriteNo];
+            animator.SetFloat("angle", angle);
 
             if (!first)
             {
                 BossAbility choice = Instantiate(abilities[2]);
                 choice.AbilityBehavior(this.gameObject);
                 first = true;
-            } else if (Time.time - lastCastTime >= castDelay)
-              {
-                    float distance = vectordist.magnitude;
-                
-                    if (distance < 5 && !hasShield) {
-                        ChooseDefensive();
-                    } else {
-                        ChooseAttack();
+            }
+            else if (Time.time - lastCastTime >= castDelay)
+            {
+                float distance = vectordist.magnitude;
+
+                if (distance < 5 && !hasShield)
+                {
+                    if (Time.time - lastTP > 12)
+                    {
+                        StartCoroutine(TeleportFromPlayer());
+                        lastTP = Time.time;
                     }
-                    lastCastTime= Time.time;
+                    else
+                    {
+                        StartCoroutine(CastingAnimation(false));
+                    }
                 }
+                else
+                {
+                    StartCoroutine(CastingAnimation(true));
+                }
+                lastCastTime = Time.time;
+            }
         }
     }
 
-    public void ActivateCrystals () {
-        foreach(GameObject crystal in crystals) {
+    IEnumerator CastingAnimation(bool atk)
+    {
+        animator.SetBool("casting", true);
+        yield return new WaitForSeconds(0.5f);
+        
+        if(atk)
+        {
+            ChooseAttack();
+        }
+        else
+        {
+            ChooseDefensive();
+        }
+        yield return new WaitForSeconds(0.5f);
+        animator.SetBool("casting", false);
+    }
+
+    public void ActivateCrystals()
+    {
+        foreach (GameObject crystal in crystals)
+        {
             crystal.SetActive(true);
         }
         shield.SetActive(true);
@@ -98,11 +132,12 @@ public class LichController : Controller
         hasShield = false;
         Enemy enemyComp = GetComponent<Enemy>();
         enemyComp.enabled = true;
-        castDelay = castDelay / 1.5f;
+        castDelay = castDelay / 1.25f;
         Destroy(shield);
     }
 
-    public bool HasShield(){
+    public bool HasShield()
+    {
         return hasShield;
     }
     public void ChooseAttack()
@@ -114,10 +149,12 @@ public class LichController : Controller
             if (i <= 15)
             {
                 i = 2;
-             } else if (i <= 75)
+            }
+            else if (i <= 65)
             {
                 i = 0;
-            } else
+            }
+            else
             {
                 i = 1;
             }
@@ -131,7 +168,8 @@ public class LichController : Controller
             if (i <= 60)
             {
                 i = 0;
-            } else
+            }
+            else
             {
                 i = 1;
             }
@@ -145,33 +183,24 @@ public class LichController : Controller
         int i = Random.Range(0, 100);
         if (i <= 35)
         {
-            i = 0;
+                i = 0;
         }
-        else if (i <= 75)
+        else
         {
             i = 1;
         }
-        else
-        {
-            i = 2;
-        }
 
-        if (i < defensiveAbilities.Count)
-        {
-            BossAbility choice = Instantiate(defensiveAbilities[i]);
-            choice.AbilityBehavior(this.gameObject);
-        }
-        else
-        {
-            TeleportFromPlayer();
-       }
+        BossAbility choice = Instantiate(defensiveAbilities[i]);
+        choice.AbilityBehavior(this.gameObject);
     }
 
     //honestly can be changed into an ability that tps to a random point later when theres time
-    private void TeleportFromPlayer()
+    private IEnumerator TeleportFromPlayer()
     {
+        animator.SetBool("tp", true);
+        yield return new WaitForSeconds(0.8f);
         currentPoint = (currentPoint + 1) % tppoints.Count;
-        print(currentPoint);
         this.transform.position = tppoints[currentPoint];
+        animator.SetBool("tp", false);
     }
 }

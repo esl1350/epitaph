b61730bfba05552bb592facc501244d09a88f5cb
Elizabeth Liu <esl1350@gmail.com>
2023-11-29 11:38:33 -0500

    hopefully this works!

--- Assets/Scripts/UI/UIAnimationEvents.cs
+++ Assets/Scripts/UI/UIAnimationEvents.cs
@@ -16,9 +16,18 @@ public class UIAnimationEvents : MonoBehaviour
     public void OnUIFadeoutEnd() {
         levelGeneration = GameObject.Find("LevelGenerator").GetComponent<LevelGeneration>();
         levelGeneration.PlaceEnemies();
-        for (int i = 0; i<enemyManager.transform.childCount; i++) {
+        for (int i = 1; i<enemyManager.transform.childCount; i++) {
             enemyManager.transform.GetChild(i).gameObject.SetActive(true);
         }
+        if (enemyManager.transform.childCount > 0) {
+            StartCoroutine(DestroyFirstChild()); // this is very bad.
+        }
+
+    }
+
+    IEnumerator DestroyFirstChild() {
+        yield return new WaitForSeconds(0.2f);
+        Destroy(enemyManager.transform.GetChild(0).gameObject);
     }
 
 }
